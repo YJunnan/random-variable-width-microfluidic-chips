92e2f40e4077d9bf3ec8458cc8876a52baa40a50
YJunnan <104745184+YJunnan@users.noreply.github.com>
2022-05-04 17:56:02 +0800

    Add files via upload

---
+++ BPNN/BPNNVelocity.py
@@ -0,0 +1,208 @@
+# coding: utf-8
+
+from time import *
+import datetime
+import torch
+import torch.nn.functional as F
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split  # ,cross_val_score,KFold
+import matplotlib.pyplot as plt
+
+
+begin_time = datetime.datetime.now()
+trainingSet = pd.read_csv('BPNN-RVW.csv', header=None)
+testSet = pd.read_csv('BPNN-RVW.csv', header=None)
+
+# 导入数据
+X = trainingSet.iloc[0:8000, 0:40]
+y = trainingSet.iloc[0:8000, 43:46]  # concentration
+test_X = testSet.iloc[8000:10232, 0:40]
+test_y = testSet.iloc[8000:10232, 43:46]
+
+print(X)
+print(y)
+print(test_X)
+print(test_y)
+# covert data to tensor
+train_X = np.array(X)
+train_X = torch.from_numpy(train_X)
+train_X = train_X.float().cuda()
+# print(train_X.dtype)
+train_y = np.array(y)
+train_y = torch.from_numpy(train_y)
+train_y = train_y.float().cuda()
+test_X = np.array(test_X)
+test_X = torch.from_numpy(test_X)
+test_X = test_X.float().cuda()
+test_y = np.array(test_y)
+test_y = torch.from_numpy(test_y)
+test_y = test_y.float().cuda()
+
+
+# construct nn
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)  # hidden layer
+        self.relu1 = torch.nn.ReLU()
+
+        self.hidden2 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu2 = torch.nn.ReLU()
+
+        self.hidden3 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu3 = torch.nn.ReLU()
+
+        self.hidden4 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu4 = torch.nn.Sigmoid()
+
+        self.hidden5 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu5 = torch.nn.Sigmoid()
+
+        self.hidden6 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu6 = torch.nn.Sigmoid()
+
+        self.hidden7 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu7 = torch.nn.Sigmoid()
+
+        self.hidden8 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu8 = torch.nn.Sigmoid()
+
+        self.hidden9 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu9 = torch.nn.Sigmoid()
+
+        self.hidden10 = torch.nn.Linear(n_hidden, n_hidden)
+        self.relu10 = torch.nn.Sigmoid()
+
+        self.bn10 = torch.nn.BatchNorm1d(25)
+        self.dp10 = torch.nn.Dropout(0.25)
+
+        self.predict = torch.nn.Linear(n_hidden, n_output)  # output layer
+        self.sigmoid = torch.nn.Sigmoid()
+    def forward(self, x):
+        x = self.hidden(x)
+        x = self.relu1(x)
+
+        x = self.hidden2(x)
+        x = self.relu2(x)
+
+        x = self.hidden3(x)
+        x = self.relu3(x)
+
+        x = self.hidden4(x)
+        x = self.relu4(x)
+
+        x = self.hidden5(x) 
+        x = self.relu5(x)
+
+        x = self.hidden6(x)
+        x = self.relu6(x)
+
+        x = self.hidden7(x)
+        x = self.relu7(x)
+
+        x = self.hidden8(x)  
+        x = self.relu8(x)
+
+        # x = self.hidden9(x) 
+        # x = self.relu9(x)
+        #
+        # x = self.hidden10(x) 
+        # x = self.relu10(x)
+
+
+        x = self.bn10(x)
+        x = self.dp10(x)
+
+        x = self.predict(x)
+        x = self.sigmoid(x)
+        return x
+
+
+
+net = Net(n_feature=40, n_hidden=25, n_output=3)
+net.cuda()
+print(net)
+
+
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.99))
+loss_func = torch.nn.MSELoss()
+
+loss_his = []
+accuracy_his = []
+train_accuracy_his = []
+test_loss_his = []
+
+
+for t in range(4000):
+    prediction = net(train_X)
+    train_accuracy = (prediction - train_y)/prediction
+    train_accuracy = 1 - torch.mean(torch.abs(torch.tensor(train_accuracy)))
+
+
+    test_prediction = net(test_X)
+    accuracy = (test_prediction - test_y) / test_prediction
+    accuracy = 1 - torch.mean(torch.abs(torch.tensor(accuracy)))
+
+
+    loss = loss_func(prediction, train_y)
+    test_loss = loss_func(test_prediction, test_y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print('train_loss:', str(loss.item()))
+    cpu_loss = loss.cpu()
+    loss_his.append(cpu_loss.data.numpy())
+    print('test_loss:', str(test_loss.item()))
+    cpu_test_loss = test_loss.cpu()
+    test_loss_his.append(cpu_test_loss.data.numpy())
+    print('Accuracy:', str(accuracy.item()))
+    cpu_accuracy = accuracy.cpu()
+    accuracy_his.append(cpu_accuracy.data.numpy())
+    cpu_train_accuracy = train_accuracy.cpu()
+    train_accuracy_his.append(cpu_train_accuracy.data.numpy())
+
+
+# train loss plot
+plt.figure()
+plt.plot(loss_his)
+plt.xlabel('Steps')
+plt.ylabel('Train Loss')
+plt.savefig('./results/velocity/velocity_net_train_loss.png')
+np.save('./results/velocity/velocity_train_loss_his.npy', loss_his)
+
+# Test loss plot
+plt.figure()
+plt.plot(test_loss_his)
+plt.xlabel('Steps')
+plt.ylabel('Test Loss')
+plt.savefig('./results/velocity/velocity_net_test_loss.png')
+np.save('./results/velocity/velocity_test_loss_his.npy', test_loss_his)
+
+# test accuracy plot
+plt.figure()
+plt.plot(accuracy_his)
+plt.xlabel('Steps')
+plt.ylabel('Accuracy')
+plt.ylim(0,1)
+plt.savefig('./results/velocity/velocity_net_test_accuracy.png')
+
+np.save('./results/velocity/velocity_test_accuracy.npy', accuracy_his)
+
+# train accuracy plot
+plt.figure()
+plt.plot(train_accuracy_his)
+plt.xlabel('Steps')
+plt.ylabel('Accuracy')
+plt.ylim(0,1)
+plt.savefig('./results/velocity/velocity_net_train_accuracy.png')
+np.save('./results/velocity/velocity_train_accuracy.npy', train_accuracy_his)
+torch.save(net, ('./results/velocity/velocity_net.pkl'))
+
+end_time = datetime.datetime.now()
+# Benchmark vs Error plot
+runtime = (end_time-begin_time).seconds
+print(end_time)
+print(runtime)
+print(test_y)
+print(test_prediction)
---
+++ KD-MiniVGGNet/VelocityNET.py
@@ -0,0 +1,307 @@
+# coding: utf-8
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as Data
+import numpy as np
+import pandas as pd
+# import torchvision
+# from sklearn.model_selection import train_test_split#,cross_val_score,KFold
+import matplotlib.pyplot as plt
+import joblib
+from sklearn.model_selection import KFold
+import sklearn.model_selection as ms
+import math
+from torch.optim import lr_scheduler
+begin_time = time.time()
+from sklearn import preprocessing
+
+
+
+EPOCH = 200
+#step_size = 100
+# train the training data n times, to save time, we just train 1 epoch
+train_BATCH_SIZE = 32
+test_BATCH_SIZE = 64
+LR = 0.0005
+
+# 操作间隔
+train_interval = 240
+test_interval = 40
+
+random_seed = 2 # 随机种子，设置后可以得到稳定的随机数
+torch.manual_seed(random_seed)
+# 导入数据
+
+data = pd.read_csv('RVW.csv', header=None)
+#X = joblib.load('randomall2D7225_shuffle.pkl')
+data1 = pd.read_csv('RVW.csv', header=None)
+physics = 'velocity'
+Y = data.iloc[:, 84:87].to_numpy()  # concentration
+#Y = Y+1
+
+X = data1.iloc[:, 0:81].to_numpy() # 读取前面保存的数组
+X = X.reshape(10232, 1, 9, 9)
+torch.set_printoptions(precision=8)
+print(X)
+print(Y)
+
+train_X, test_X, train_y, test_y = ms.train_test_split(X, Y, test_size=0.25, random_state = 32)
+train_X = torch.from_numpy(train_X)
+train_y = torch.from_numpy(train_y)
+test_X = torch.from_numpy(test_X)
+test_y = torch.from_numpy(test_y)
+#np.savetxt('test_X_V.csv', test_X.reshape(2558,81), delimiter = ',')
+#np.savetxt('test_y_V.csv', test_y.reshape(2558,3), delimiter = ',')
+#np.savetxt('train_X_V.csv', train_X.reshape(7674,81), delimiter = ',')
+#np.savetxt('train_y_V.csv', train_y.reshape(7674,3), delimiter = ',')
+test_X = (test_X).cuda()
+test_y = (test_y).cuda()
+train_X = (train_X).cuda()
+train_y = (train_y).cuda()
+train_torch_dataset = Data.TensorDataset(train_X, train_y)
+test_torch_dataset = Data.TensorDataset(test_X, test_y)
+
+
+train_loader = Data.DataLoader(
+    dataset=train_torch_dataset,      # 数据，封装进Data.TensorDataset()类的数据
+    batch_size=train_BATCH_SIZE,      # 每块的大小
+    shuffle=True,               # 要不要打乱数据 (打乱比较好)
+    num_workers=0,              # 多进程（multiprocess）来读数据
+)
+
+test_loader = Data.DataLoader(
+    dataset=test_torch_dataset,      # 数据，封装进Data.TensorDataset()类的数据
+    batch_size=test_BATCH_SIZE,      # 每块的大小
+    shuffle=True,               # 要不要打乱数据 (打乱比较好)
+    num_workers=0,              # 多进程（multiprocess）来读数据
+)
+class GooGleNet(nn.Module):
+    def __init__(self, num_classes=3, init_weights=False):
+        super(GooGleNet, self).__init__()
+        self.features = nn.Sequential(  #打包
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,9,9)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(0, 0)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,8,8)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,9,9)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(0, 0)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,8,8)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,9,9)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(0, 0)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,8,8)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,9,9)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(0, 0)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,8,8)
+            nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            # (32,9,9)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # (32,4,4)
+            nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            # (64,4,4)
+            nn.Conv2d(64, 64, kernel_size=(2, 2), stride=(1, 1), padding=(0, 0)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            # (64,3,3)
+            nn.Conv2d(64, 64, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            # (64,4,4)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # (64,2,2)
+            # nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(32),
+            #
+            # nn.Conv2d(32, 32, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(32),
+            #
+            # nn.Conv2d(32, 32, kernel_size=(2,2), stride=(1,1),padding=(0,0)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(32),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+            #
+            # nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(64),
+            #
+            # nn.Conv2d(64, 64, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(64),
+            #
+            # nn.Conv2d(64, 64, kernel_size=(2, 2), stride=(1, 1), padding=(0, 0)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(64),
+            #
+            # nn.Conv2d(64,128, kernel_size=3, stride=(1, 1), padding=(1, 1)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(128),
+            #
+            # nn.Conv2d(128, 128, kernel_size=(2, 2), stride=(1, 1), padding=(1, 1)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(128),
+            #
+            # nn.Conv2d(128, 128, kernel_size=(2, 2), stride=(1, 1), padding=(0, 0)),
+            # nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(128),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            # 全链接
+            nn.Linear(64 * 2 * 2,128),
+            nn.ReLU(),
+            # nn.Dropout(p=0.5),
+            nn.Linear(128, num_classes),
+            nn.Sigmoid()
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, start_dim=1) # 展平或者view()
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu') #何教授方法
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)  # 正态分布赋值
+                nn.init.constant_(m.bias, 0)
+cnn = GooGleNet()
+cnn = cnn.cuda()
+print(cnn)  # net architecture
+
+#optimizer = torch.optim.Adam(cnn.parameters(),lr = LR)
+optimizer = torch.optim.Adam(cnn.parameters(),lr = LR)
+#optimizer = torch.optim.SGD(cnn.parameters(), lr=LR, momentum=0.5, dampening=0.5, weight_decay=0.01, nesterov=False)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,[50,100],gamma=0.2,last_epoch=-1)
+loss_func = nn.MSELoss()
+cnn = cnn.double()
+
+# training
+loss_his = []
+test_accuracy_his = []
+train_accuracy_his = []
+test_loss_his = []
+save_acc = []
+save_loss = []
+save_acc_train = []
+save_loss_train = []
+
+def train(eopchs):
+    cnn.train()
+    loss_cu = 0
+    train_acc_cu = 0
+    for steps, (batch_x, batch_y) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = cnn(batch_x)
+        loss = loss_func(batch_y,output)
+        loss.backward()
+        optimizer.step()
+        train_accuracy = (batch_y-output) / output
+        train_accuracy = (1 - torch.mean(torch.abs(torch.as_tensor(train_accuracy))))
+        loss_cu += loss
+        train_acc_cu += train_accuracy
+    loss_cu = loss_cu/train_interval
+    train_acc_cu = train_acc_cu/train_interval
+    loss_cu = loss_cu.cpu()
+    train_acc_cu = train_acc_cu.cpu()
+    print(loss_cu,train_acc_cu)
+    loss_his.append(loss_cu.data.numpy())
+    train_accuracy_his.append(train_acc_cu.data.numpy())
+
+def test(eopchs):
+    cnn.eval()
+    test_loss_cu = 0
+    test_acc_cu = 0
+    with torch.no_grad():
+        for test_batch_x, test_batch_y in test_loader:
+            test_prediction = cnn(test_batch_x)  # 传入这一组 batch，进行前向计算
+            test_loss = loss_func(test_batch_y,test_prediction)
+            test_accuracy = (test_batch_y-test_prediction) /test_prediction
+            test_accuracy = (1 - torch.mean(torch.abs(torch.as_tensor(test_accuracy))))
+            test_acc_cu += test_accuracy
+            test_loss_cu += test_loss
+        test_acc_cu = test_acc_cu / test_interval
+        test_loss_cu = test_loss_cu / test_interval
+        cpu_test_loss = test_loss_cu.cpu()
+        test_cpu_accuracy = test_acc_cu.cpu()
+        print(cpu_test_loss, test_cpu_accuracy)
+        test_accuracy_his.append(test_cpu_accuracy.data.numpy())
+        test_loss_his.append(cpu_test_loss.data.numpy())
+        if test_accuracy_his[-1] >= max(test_accuracy_his):
+            # print('save network')
+            torch.save(cnn, ('./' + physics + '/' + str(begin_time) + 'net.pkl'))
+            torch.save(cnn.state_dict(), './model02.pth')
+
+for epochs in range(1, EPOCH + 1):
+    print('EPOCH:',epochs)
+    train(epochs)
+    test(epochs)
+    #scheduler.step()
+
+
+# train loss plot
+plt.figure()
+plt.plot(loss_his)
+plt.xlabel('Steps')
+plt.ylabel('Train Loss')
+plt.savefig('./' + physics + '/' + str(begin_time) + 'train_loss.png')
+np.save('./' + physics + '/' + str(begin_time) + 'train_loss.npy', loss_his)
+
+# Test loss plot
+plt.figure()
+plt.plot(test_loss_his)
+plt.xlabel('Steps')
+plt.ylabel('Test Loss')
+plt.savefig('./' + physics + '/' + str(begin_time) + 'test_loss.png')
+np.save('./' + physics + '/' + str(begin_time) + 'test_loss.npy', test_loss_his)
+
+# accuracy plot
+plt.figure()
+plt.plot(test_accuracy_his)
+plt.xlabel('Steps')
+plt.ylabel('test Accuracy')
+plt.ylim(0,1)
+plt.savefig('./' + physics + '/' + str(begin_time) + 'test_accuracy.png')
+np.save('./' + physics + '/' + str(begin_time) + 'test_accuracy.npy', test_accuracy_his)
+
+plt.figure()
+plt.plot(train_accuracy_his)
+plt.xlabel('Steps')
+plt.ylabel('train Accuracy')
+plt.ylim(0,1)
+plt.savefig('./' + physics + '/' + str(begin_time) + 'train_accuracy.png')
+np.save('./' + physics + '/' + str(begin_time) + 'train_accuracy.npy', train_accuracy_his)
+
+
